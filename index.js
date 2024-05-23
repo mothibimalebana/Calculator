f49a38b21ffc00b93c01987d4447cbdd4a73f0a3
mothibimalebana <mothibi.mm12@gmail.com>
2024-05-23 19:58:02 +0200

    airthmetic operators display on screen when clicked
    User can see the airithmetic operators they clicked on the screen, to avoid mistaked

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const inputScreen = document.querySelector(".input");
+let num1;
+let num2;
 
 function displayToScreen (num){
 	inputScreen.textContent += num
 }
 
+//numberPad
 const btn7 = document.querySelector("#btn7 button");
 btn7.addEventListener("click",() => {displayToScreen(7)});
 
@@ -31,3 +34,24 @@ btn6.addEventListener("click",() => {displayToScreen(6)});
 
 const btn3 = document.querySelector("#btn3 button");
 btn3.addEventListener("click",() => {displayToScreen(3)});
+
+//operationPad
+const btnMinus = document.querySelector("#btnMinus button");
+btnMinus.addEventListener("click",() => {displayToScreen("-")});
+
+const btnAdd = document.querySelector("#btnAdd button");
+btnAdd.addEventListener("click",() => {displayToScreen("+")});
+
+const btnMultiply = document.querySelector("#btnMultiply button");
+btnMultiply.addEventListener("click",() => {displayToScreen("*")});
+
+
+const btnDivide = document.querySelector("#btnDivide button");
+btnDivide.addEventListener("click",() => {displayToScreen("/")});
+
+
+const btnEquals = document.querySelector("#btnEquals button");
+btnEquals.addEventListener("click",() => {displayToScreen("=")});
+
+const btnAC = document.querySelector("#btnAC button");
+btnAC.addEventListener("click",() => {inputScreen.textContent = " "});
