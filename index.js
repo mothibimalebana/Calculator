47c19b1c0f293cfa8e4150e55f51319751ba34ce
mothibimalebana <mothibi.mm12@gmail.com>
2024-05-23 19:16:37 +0200

    renamed function calc to displayToScreen
    at first the function was to calculate the input but, it diverged into a displayToScreen function

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const inputScreen = document.querySelector(".input");
 
-function calc (num){
+function displayToScreen (num){
 	inputScreen.textContent += num
 }
 
 const btn7 = document.querySelector("#btn7 button");
-btn7.addEventListener("click",() => {calc(7)});
+btn7.addEventListener("click",() => {displayToScreen(7)});
 
 const btn4 = document.querySelector("#btn4 button");
-btn4.addEventListener("click",() => {calc(4)});
+btn4.addEventListener("click",() => {displayToScreen(4)});
 
 const btn1 = document.querySelector("#btn1 button");
-btn1.addEventListener("click",() => {calc(1)});
+btn1.addEventListener("click",() => {displayToScreen(1)});
 
 
 const btn8 = document.querySelector("#btn8 button");
-btn8.addEventListener("click",() => {calc(8)});
+btn8.addEventListener("click",() => {displayToScreen(8)});
 
 const btn5 = document.querySelector("#btn5 button");
-btn5.addEventListener("click",() => {calc(5)});
+btn5.addEventListener("click",() => {displayToScreen(5)});
 
 const btn2 = document.querySelector("#btn2 button");
-btn2.addEventListener("click",() => {calc(2)});
+btn2.addEventListener("click",() => {displayToScreen(2)});
 
 const btn9 = document.querySelector("#btn9 button");
-btn9.addEventListener("click",() => {calc(9)});
+btn9.addEventListener("click",() => {displayToScreen(9)});
 
 const btn6 = document.querySelector("#btn6 button");
-btn6.addEventListener("click",() => {calc(6)});
+btn6.addEventListener("click",() => {displayToScreen(6)});
 
 const btn3 = document.querySelector("#btn3 button");
-btn3.addEventListener("click",() => {calc(3)});
+btn3.addEventListener("click",() => {displayToScreen(3)});
