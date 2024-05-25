7643b61daf1c56cbd75fe7878cad6894abdd4e2b
mothibimalebana <mothibi.mm12@gmail.com>
2024-05-25 04:22:32 +0200

    100% complete

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
 const inputScreen = document.querySelector(".input");
 let num1;
 let num2;
+let regEx;
+let ans;
+let operation;
 
 //functions
+	//screen operations
 function displayToScreen (num){
 	inputScreen.textContent += num
 }
+function clearScreen(){
+	inputScreen.textContent = " "
+}
 function getNum1 (inputDisplay){
-	num1 = inputDisplay;
+	num1 = Number(inputDisplay.slice(0));
 }
-function getNum2 (inputDisplay){
-	num2 = inputDisplay;
-}
+function getRegex (inputDisplay){
+	evaluateOperation(inputDisplay);
 
+}
+	//mathematical operations
+function add(num1, num2){
+	return num1 + num2
+}
+function minus(num1, num2){
+	return num1 - num2
+}
+function multiply(num1, num2){
+	return num1 * num2
+}
+function divide(num1, num2){
+	return num1 / num2
+}
+function convertToNumber(string){
+	return Number(string)
+}
+function determineOperation(operation){
+	switch(operation){
+		case '+':
+			return ans = add(num1, num2)
+		break;
+	
+		case '-':
+			return ans = minus(num1, num2)
+		break;
+
+		case '×':
+			return ans = multiply(num1, num2)
+		break;
+	
+		case '÷':
+			return ans = divide(num1, num2)
+		break;
+	}
+}
+	//evaluate operation
+function evaluateOperation(regEx){
+	operation = regEx.slice(1,2);
+	num2 = Number(regEx.slice(2));
+	determineOperation(operation);
+}
 //numberPad
 const btn7 = document.querySelector("#btn7 button");
 btn7.addEventListener("click",() => {displayToScreen(7)});
@@ -47,21 +95,21 @@ btn3.addEventListener("click",() => {displayToScreen(3)});
 const btnMinus = document.querySelector("#btnMinus button");
 btnMinus.addEventListener("click",() => {
 	getNum1(inputScreen.textContent);
-	inputScreen.textContent = " ";
+	clearScreen();
 	displayToScreen("-")
 });
 
 const btnAdd = document.querySelector("#btnAdd button");
 btnAdd.addEventListener("click",() => {
 	getNum1(inputScreen.textContent);
-	inputScreen.textContent = " ";
+	clearScreen();
 	displayToScreen("+")
 });
 
 const btnMultiply = document.querySelector("#btnMultiply button");
 btnMultiply.addEventListener("click",() => {
 	getNum1(inputScreen.textContent);
-	inputScreen.textContent = " ";
+	clearScreen();
 	displayToScreen("×")
 });
 
@@ -69,12 +117,17 @@ btnMultiply.addEventListener("click",() => {
 const btnDivide = document.querySelector("#btnDivide button");
 btnDivide.addEventListener("click",() => {
 	getNum1(inputScreen.textContent);
-	inputScreen.textContent = " ";
+	clearScreen();
 	displayToScreen("÷")
 });
 
 
 const btnEquals = document.querySelector("#btnEquals button");
+btnEquals.addEventListener("click",() => {
+	evaluateOperation(inputScreen.textContent);
+	clearScreen();
+	inputScreen.textContent = ans;
+})
 
 const btnAC = document.querySelector("#btnAC button");
 btnAC.addEventListener("click",() => {inputScreen.textContent = " "});
