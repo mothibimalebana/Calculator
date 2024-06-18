ee1e96b8df834872c8a4e73e23a856b8010a63e4
mothibimalebana <mothibi.mm12@gmail.com>
2024-06-18 04:02:16 +0200

    created varaibles: num1, num2, op
    These wil be used later to update our display

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//variables
+let num1, num2, op;
+
 //arithmetic functions
 function add(num1, num2) {
 	return num1 + num2
@@ -10,4 +13,9 @@ function multiply(num1, num2) {
 }
 function divide(num1, num2) {
 	return num1 / num2
+}
+
+//operation function
+operation(op, num1, num2){
+	switch()
 }
