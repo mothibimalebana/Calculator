5b2a9f40a907083263518d6535e28935d8ea726e
mothibimalebana <mothibi.mm12@gmail.com>
2024-06-19 10:39:40 +0200

    calcKeyboard is now functional
    user inputs get displayed to the screen upon clicking

--- index.js
+++ index.js
@@ -1,18 +1,100 @@
 //variables
-let num1, num2, op;
+let num1, num2, op, regEx;
+let operatorsArray = ['+', '*', '-', '/'];
+let numberArray = ['0','1','2','3','4','5','6','7','8','9'];
+
+/*Screen*/
 let inputScreen = document.querySelector(".inputScreen");
+/*Keyboard*/
+//first row
+let C = document.querySelector("#C");
+let M = document.querySelector("#M");
+let numPerc = document.querySelector('#numPerc');
+let numDivide = document.querySelector('#numDivide');
+//second row
+let no7 = document.querySelector("#no7");
+let no8 = document.querySelector("#no8");
+let no9 = document.querySelector("#no9");
+let nox = document.querySelector("#nox");
+//third row
+let no4 = document.querySelector('#no4');
+let no5 = document.querySelector('#no5');
+let no6 = document.querySelector('#no6');
+let numSubtract = document.querySelector('#numSubtract')
+//fourth row
+let no1 = document.querySelector('#no1');
+let no2 = document.querySelector('#no2');
+let no3 = document.querySelector('#no3');
+let numAdd = document.querySelector('#numAdd');
+//fifth row
+let decimal = document.querySelector('#decimal');
+let no0 = document.querySelector('#no0');
+let del = document.querySelector('#del');
+let numEquals = document.querySelector('#numEquals');
+
+no7.addEventListener("click", () => getRegEx(7))
+no8.addEventListener("click", () => getRegEx(8))
+no9.addEventListener("click", () => getRegEx(9))
+no4.addEventListener("click", () => getRegEx(4))
+no5.addEventListener("click", () => getRegEx(5))
+no6.addEventListener("click", () => getRegEx(6))
+no1.addEventListener("click", () => getRegEx(1))
+no2.addEventListener("click", () => getRegEx(2))
+no3.addEventListener("click", () => getRegEx(3))
+no0.addEventListener("click", () => getRegEx(0))
+
+
+C.addEventListener("click", () => clear())
+decimal.addEventListener("click", () => getRegEx('.'))
+numAdd.addEventListener("click", () => getRegEx('+'))
+numSubtract.addEventListener("click", () => getRegEx('-'))
+numDivide.addEventListener("click", () => getRegEx('/'))
+nox.addEventListener("click", () => getRegEx('x'))
+function getRegEx(num){
+	inputScreen.textContent += num
+}
+function clear(){
+	num1 = " "
+	num2 = " "
+	op = " "
+	inputScreen.textContent = " "
+}
+function stringToNumber(string){
+	return num1 = Number(num1);
+}
+function spliceRegEx(regEx){
+	op = regEx[0].slice(0);
+	num1 = regEx.slice(1);
+	stringToNumber(num1);
+}
+function isOperator(regEx){
+	if (operatorsArray.includes(regEx[0])){
+		spliceRegEx(regEx);
+	}
+	else{
+		stringToNumber(regEx)
+	}
+}
+function isNumber(regex){
+	if(numberArray.includes(regex)){
+		return true
+	}
+	else{
+		return false
+	}
+}
 
 //arithmetic functions
-function add(num1, num2) {
+function add(num1, num2){
 	return num1 + num2
 }
-function subtract(num1, num2) {
+function subtract(num1, num2){
 	return num1 - num2
 }
-function multiply(num1, num2) {
+function multiply(num1, num2){
 	return num1 * num2
 }
-function divide(num1, num2) {
+function divide(num1, num2){
 	return num1 / num2
 }
 
@@ -20,19 +102,34 @@ function divide(num1, num2) {
 function operate(op, num1, num2){
 	switch(op){
 		case '+':
+			if(num2 === undefined){
+				num2 = 0;
+			}
 			result = add(num1,num2)
 			break;
 		case '-':
+			if(num2 === undefined){
+				num2 = 0;
+			}
 			result = subtract(num1,num2)
 			break;
 		case '*':
+			if(num2 === undefined){
+				num2 = 1;
+			}
 			result = multiply(num1,num2)
 			break;
 		case '/':
+			if(num2 === undefined){
+				num2 = 1;
+			}
 			result = divide(num1,num2)
 			break;
 		default:
 			alert("invalid operator");
 	}
-	return result;
+	displayResult(result);
+}
+function displayResult(result){
+	inputScreen.textContent = result;
 }
